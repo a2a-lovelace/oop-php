28acfe5e7c4761b2abb4a5b0d7b09128193ce29a
Ada <science.pika@gmail.com>
2024-11-05 12:59:15 -0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ require_once '../classes/Department.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Система управления пользователями</title>
+    <link rel="stylesheet" href="../css/styles.css"> <!-- путь к CSS стилям, цветам -->
 </head>
 <body>
     <h1>Система управления пользователями</h1>
