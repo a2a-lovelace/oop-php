25ee3f079d4b4d9fca6a73353e74659b23b34c17
Ada <science.pika@gmail.com>
2024-11-06 04:08:18 -0900

    Update 2bdConnection.php

--- classes/2bdConnection.php
+++ classes/2bdConnection.php
@@ -9,4 +9,22 @@ class bdConnection {
     private static $bdPass = 'your_password';
     private static $bdHost = 'localhost';
     private static $bdDataBase = 'your_database';
+
+    private function __construct() {
+        $this->connect();
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new bdConnection();
+        }
+        return self::$instance;
+    }
+
+    private function connect() {
+        $this->connection = new mysqli(self::$bdHost, self::$bdLogin, self::$bdPass, self::$bdDataBase);
+        if ($this->connection->connect_error) {
+            $this->error = "Ошибка соединения: " . $this->connection->connect_error;
+        }
+    }
 }
