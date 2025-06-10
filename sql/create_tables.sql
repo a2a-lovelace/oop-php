4768bb631455a8dda5abb0c89ddf1c7afe36b0ae
Ada <science.pika@gmail.com>
2025-06-10 23:00:19 +0300

    Update create_tables.sql
    fixed

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -8,22 +8,22 @@ CREATE TABLE users (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE testdepartments (
+CREATE TABLE departments (
     id INT(11) NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE test_users_departments (
+CREATE TABLE users_departments (
     post_id INT(11) NOT NULL AUTO_INCREMENT,
     user_id INT(11) DEFAULT NULL,
     department_id INT(11) DEFAULT NULL,
     post VARCHAR(50) DEFAULT NULL,
     PRIMARY KEY (post_id),
-    FOREIGN KEY (user_id) REFERENCES testusers(id) 
+    FOREIGN KEY (user_id) REFERENCES users(id) 
         ON UPDATE RESTRICT 
         ON DELETE RESTRICT,
-    FOREIGN KEY (department_id) REFERENCES testdepartments(id) 
+    FOREIGN KEY (department_id) REFERENCES departments(id) 
         ON UPDATE RESTRICT 
         ON DELETE RESTRICT
 );
