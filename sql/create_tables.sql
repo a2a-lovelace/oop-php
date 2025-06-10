e2eb71a0d1b903b9b3d3b841307c966315e5939c
Ada <science.pika@gmail.com>
2025-06-10 22:31:28 +0300

    Update create_tables.sql
    added my script for MariaDB

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,21 +1,29 @@
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) DEFAULT NULL,
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    fio JSON,
-    role VARCHAR(50)
+    password_hash VARCHAR(255) NOT NULL,
+    fio VARCHAR(255) DEFAULT NULL,
+    role VARCHAR(50) DEFAULT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE departments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+CREATE TABLE testdepartments (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE users_departments (
-    user_id INT,
-    department_id INT,
-    part VARCHAR(50),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+CREATE TABLE test_users_departments (
+    post_id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11) DEFAULT NULL,
+    department_id INT(11) DEFAULT NULL,
+    post VARCHAR(50) DEFAULT NULL,
+    PRIMARY KEY (post_id),
+    FOREIGN KEY (user_id) REFERENCES testusers(id) 
+        ON UPDATE RESTRICT 
+        ON DELETE RESTRICT,
+    FOREIGN KEY (department_id) REFERENCES testdepartments(id) 
+        ON UPDATE RESTRICT 
+        ON DELETE RESTRICT
 );
